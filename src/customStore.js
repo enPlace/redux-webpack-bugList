e302c88303d5556eee1e38c3b1753d1953021c21
enPlace <n.place@protonmail.com>
2021-09-01 17:52:16 -0300

    Create subscriber method that pushes methods to an array and calls each on state change.

--- src/customStore.js
+++ src/customStore.js
@@ -2,17 +2,26 @@ import reducer from "./Reducer";
 
 function createStore(reducer) {
   let state;
+  let listeners = [];
 
   function getState() {
-      //getter method
+    //getter method
     return state;
   }
 
   function dispatch(action) {
-      //call the reducer to get the new state
-    state = reducer(state, action)
+    //call the reducer to get the new state
+    state = reducer(state, action);
+
+    for (let i = 0; i < listeners.length; i++) {
+      console.log(listeners[i]);
+      listeners[i]();
+    }
   }
 
-  return { getState, dispatch };
+  function subscribe(listener) {
+    listeners.push(listener);
+  }
+  return { getState, dispatch, subscribe };
 }
 export default createStore(reducer);
--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-import store from "./customStore"
+import store from "./customStore";
+import * as actions from "./Actions";
+
+store.subscribe(()=>{
+    console.log("store changed!")
+})
+store.subscribe(()=>console.log("hello"))
+store.dispatch(actions.bugAdded("bug 1"));
 
 console.log(store.getState())
