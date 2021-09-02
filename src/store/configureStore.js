4b0de8a75f1441be85ff03c592f039a1c5d74e04
enPlace <n.place@protonmail.com>
2021-09-02 18:34:13 -0300

    Use redux toolkit

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,6 @@
-import { createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./bugs";
-import { devToolsEnhancer } from "redux-devtools-extension";
 
-export default function configureStore(){
-  const store = createStore(
-    reducer /* preloadedState, */,
-    devToolsEnhancer({trace:true})
-  );
-  return store
+export default function () {
+  return configureStore({ reducer });
 }
